f5e9aed6871836eb53c09961e4e7f613ee8c9708
Denis Vlašiček <denis.vlasicek@gmail.com>
2022-03-27 19:34:00 +0200

    seemingly working generator fun

--- helpers/party-sampler.jl
+++ helpers/party-sampler.jl
@@ -5,12 +5,34 @@ function makePartyName(transition_probs::Dict{String, Any},
                        initial_probs::Dict{String, Any})
     party_name = Vector()
 
-    words = collect(keys(initial_probs))
+    initial_words = collect(keys(initial_probs))
 
     initial_weights = StatsBase.Weights(Vector{Float64}(undef,
-                                                        length(words)))
+                                                        length(initial_words)))
 
-    for (i, word) in enumerate(words)
+    for (i, word) in enumerate(initial_words)
         initial_weights[i] = initial_probs[word]
     end
+
+    append!(party_name,
+            [StatsBase.sample(initial_words,
+                              initial_weights)])
+
+    while !isempty(transition_probs[last(party_name)])
+        words_next = collect(keys(transition_probs[last(party_name)]))
+
+        weights_next = StatsBase.Weights(Vector{Float64}(undef,
+                                                         length(words_next)))
+
+        for (i, word) in enumerate(words_next)
+            weights_next[i] = transition_probs[last(party_name)][word]
+        end
+
+        append!(party_name,
+                [StatsBase.sample(words_next,
+                                  weights_next)])
+    end
+
+    return join(party_name,
+                " ")
 end
