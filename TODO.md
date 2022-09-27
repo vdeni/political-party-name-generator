34832ac04b30e9b3b90f78ac85410b0fc4aa06a8
vdeni <denis.vlasicek@gmail.com>
2022-09-27 18:23:13 +0200

    add hardcoded words to syntactic patterns

--- TODO.md
+++ TODO.md
@@ -1,6 +1,9 @@
 - [x] extract name patterns using classla
-- [ ] collect lexicons for populating syntax patterns
-- [ ] make Flask app for generating party names
+- [x] collect lexicons for populating syntax patterns
+- [ ] make web app for generating party names
 - [x] calculate transition probabilities (rowsums = 1)
 - [x] write probabilities to JSON
 - [x] make party generating script
+- [ ] force more frequent usage of terms like 'stranka' ('political party'),
+    'zajednica' etc. try probabilistically seeding the terms through the
+    patterns
--- analysis/extract-syntactic-patterns.py
+++ analysis/extract-syntactic-patterns.py
@@ -5,25 +5,43 @@ import pandas
 import classla
 
 # prepare classla
-nlp = classla.Pipeline('hr',
-                       processors='tokenize,pos')
+nlp = classla.Pipeline("hr", processors="tokenize,pos")
 
 # import party names
-party_names = pandas.read_json(os.path.join('data',
-                                            'party-names_clean',
-                                            'stranke_imena.json'),
-                               typ='series')
+party_names = pandas.read_json(
+    os.path.join("data", "party-names_clean", "stranke_imena.json"), typ="series"
+)
 
 # flatten names with join
-party_names = party_names.map(lambda x: ' '.join(x))
+party_names = party_names.map(lambda x: " ".join(x))
 
 # process party names with classla
 docs = party_names.to_list()
 
 docs = [nlp(doc) for doc in docs]
 
-# extract unique syntactic patterns
-docs_pos = [doc.get('xpos') for doc in docs]
+# extract unique syntactic patterns, but keep some words hardcoded
+words_keep = [
+    "STRANKA",
+    "ZAJEDNICA",
+    "POKRET",
+    "SABOR",
+    "SAVEZ",
+    "UNIJA",
+    "PARTIJA",
+    "AKCIJA",
+    "FORUM",
+    "BLOK",
+    "LISTA",
+]
+
+docs_pos = [
+    [
+        elem.get("text") if elem.get("text") in words_keep else elem.get("xpos")
+        for elem in doc.to_dict()[0][0]
+    ]
+    for doc in docs
+]
 
 docs_pos_unique = list()
 
@@ -35,12 +53,12 @@ for doc in docs_pos:
 docs_filtered = list()
 
 for doc in docs_pos_unique:
-    if not any([postag in ['Xf', 'Y'] for postag in doc]):
+    if not any([postag in ["Xf", "Y"] for postag in doc]):
         docs_filtered.append(doc)
 
 # write to file
-with open(os.path.join('data',
-                       'syntactic-patterns',
-                       'syntactic-patterns.txt'), 'w') as ofile:
+with open(
+    os.path.join("data", "syntactic-patterns", "syntactic-patterns.txt"), "w"
+) as ofile:
     for doc in docs_filtered:
-        ofile.write(','.join(doc) + '\n')
+        ofile.write(",".join(doc) + "\n")
